e7a23cc06d76afa79ae35f3f3f5ced27bb67e3f9
Stevan Dragović <stevandragovic.10@gmail.com>
2024-11-13 23:16:22 +0100

    Added one example for proxy pattern

---
+++ proxy/createProxy.ts
@@ -0,0 +1,21 @@
+function createProxy(subject) {
+    const proto = Object.getPrototypeOf(subject);
+
+    function Proxy(subject) {
+        this.subject = subject;
+    }
+
+    Proxy.prototype = Object.create(proto);
+
+    Proxy.prototype.hello = function(){
+        return this.subject.hello() + 'to all';
+    }
+
+    Proxy.prototype.goodbye = function(){
+        return this.subject.goodbye.apply(this.subject, arguments);
+    }
+
+    return new Proxy(subject);
+}
+
+module.exports = createProxy;
